8451b4dc38a9a822bf15fe858d5794f098c49c94
Jaga <jagadeeshmouthouvel@gmail.com>
2022-12-14 12:50:21 +0530

    Order model and payment choices created

--- estore/store/models.py
+++ estore/store/models.py
@@ -10,9 +10,33 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE,"Bronze"),
+        (MEMBERSHIP_SILVER,"Silver"),
+        (MEMBERSHIP_GOLD,"Gold"),
+    ]
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=50)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
     
+class Order(models.Model):
+    PAYMENTSTATUS_PENDING = 'P'
+    PAYMENTSTATUS_COMPLETE = 'C'
+    PAYMENTSTATUS_FAILED = 'F'
+
+    PAYMENTSTATUS_CHOICES = [
+        (PAYMENTSTATUS_PENDING, 'Pending'),
+        (PAYMENTSTATUS_COMPLETE, 'Complete'),
+        (PAYMENTSTATUS_FAILED, 'Failed'),
+    ]
+
+    placed_at = models.DateField(auto_now_add=True, null=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENTSTATUS_CHOICES, default=PAYMENTSTATUS_PENDING)
