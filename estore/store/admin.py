06474a5c932afe92c3d54cb23ae7137778273f5c
Jaga <jagadeeshmouthouvel@gmail.com>
2023-01-14 12:40:54 +0530

    models registered

--- estore/store/admin.py
+++ estore/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Collection, Promotion
+
+admin.site.register(Product)
+admin.site.register(Collection)
+admin.site.register(Promotion)
